818fe2bd500c40fec0ba5947ae0e31d5071627fb
kol-apo <e.olubanjo@alustudent.com>
2026-01-23 14:06:41 +0200

    final

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,25 @@
+# users/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-from users.models import User, Passenger
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'first_name', 'last_name', 'user_type', 'is_staff')
+    list_filter = ('user_type', 'is_staff', 'is_superuser')
+    ordering = ('email',)
+    search_fields = ('email','first_name','last_name')
 
-admin.site.register(User)
-admin.site.register(Passenger)
+    fieldsets = (
+        (None, {'fields': ('email','password')}),
+        ('Personal info', {'fields': ('first_name','last_name','phone','user_type')}),
+        ('Permissions', {'fields': ('is_active','is_staff','is_superuser','groups')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email','password1','password2','user_type'),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
